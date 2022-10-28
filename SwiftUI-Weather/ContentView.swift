57036d6afd34b28e1dde8f8faf449582906f1e91
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2022-10-28 14:32:23 -0500

    Add xcodeproj; add city name, SF sun symbol, and temperature views

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Samantha Cannillo on 10/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.blue, .white],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Chicago, IL")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("72°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
